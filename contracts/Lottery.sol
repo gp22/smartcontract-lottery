74706201a181170b0296e6f47d11cf33381d752a
Paul Garcia <gp22@users.noreply.github.com>
2023-05-01 09:48:28 -0600

    Add chainlink VRF consumer

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,10 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract Lottery {
-    function enterLottery() returns () {}
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-    function pickRandomWinner() returns () {}
+error Lottery__NotEnoughEthEntered();
 
-    constructor() {}
+contract Lottery is VRFConsumerBaseV2 {
+    // State variables
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    // Events
+    event LotteryEntered(address indexed player);
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterLottery() public payable {
+        if (msg.value < i_entranceFee) revert Lottery__NotEnoughEthEntered();
+        s_players.push(payable(msg.sender));
+        // Emit an event when we update a dynamic array or mapping.
+        emit LotteryEntered(msg.sender);
+    }
+
+    function requestRandomWinner() external returns (uint256) {}
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    // View / Pure functions
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayers(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
 }
